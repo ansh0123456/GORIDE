82c5630cb4ce3be80749c6a3ae291439eada9758
taneja2811 <taneja2811@gmail.com>
2026-03-10 23:05:04 +0530

    Initial Commit

---
+++ GoRideTests/GoRideTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoRideTests.swift
+//  GoRideTests
+//
+//  Created by Ansh Taneja on 10/03/26.
+//
+
+import Testing
+@testable import GoRide
+
+struct GoRideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
